a301352c950315d6fbf041f74671e111c0312413
Pathomratan <Pathomrat.an@hotmail.com>
2026-05-04 12:14:27 +0700

    SeparatNavbar,Buttonmenu

--- react-app-starter-main/src/App.jsx
+++ react-app-starter-main/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Menunav from './component/Menunav'
+import Buttonmenu from './component/Buttonmenu'
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Menunav />} />
+        <Route path="/" element={<Buttonmenu />} />
         {/* คุณสามารถเพิ่ม Route อื่นๆ เช่น /menu หรือ /order ได้ที่นี่ */}
-        <Route path="/menu" element={<Menunav />} />
-        <Route path="/order" element={<Menunav />} />
+        <Route path="/menu" element={<Buttonmenu />} />
+        <Route path="/order" element={<Buttonmenu />} />
       </Routes>
     </Router>
   )
 }
-
